013beaf078c97f3f968b7baf41ad39467c59e7b7
sushmitha <vengalreddysushmitha@gmail.com>
2022-09-17 12:18:10 -0700

    [SUSHMITHA]ADDED:Returning the Rides

--- CabInvoiceGenerator/src/main/java/com/bridzelab/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/com/bridzelab/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -1,11 +1,11 @@
 package com.bridzelab.CabInvoiceGenerator;
-
 /**
  * Creating CabInvoiceGenerator class for invoice generator for the given distance and time
  */
 public class CabInvoiceGenerator {
     private static final int MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
+    private static final int MIN_FARE = 5;
 
     /**
      * Creating calculateFare to calculate the fare for the given distance and time
@@ -13,30 +13,29 @@ public class CabInvoiceGenerator {
      * @param time - int time
      * @return total fare
      */
-    public static double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare, MIN_FARE);
     }
 
     /**
-     * calculate the aggregatetotal for all
-     * @param rides
-     * @return
+     * This method is created to find the totalFare for given multiple rides
+     * @param rides an array of Rides
+     * @return InvoiceSummary
      */
-
-    public double calculateFare(Ride[] rides) {
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride : rides) {
-
             totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
         }
-        return totalFare;
+        return new InvoiceSummary(rides.length, totalFare);
     }
+
     /**
      * Main method to print Welcome message
      * @param args
      */
     public static void main(String[] args) {
         System.out.println("Welcome to CabInvoiceGenerator Application!");
-        System.out.println(calculateFare(2.0, 5));
     }
 }
--- CabInvoiceGenerator/src/test/java/com/bridzelab/CabInvoiceGenerator/CabInvoiceGeneratorTest.java
+++ CabInvoiceGenerator/src/test/java/com/bridzelab/CabInvoiceGenerator/CabInvoiceGeneratorTest.java
@@ -1,15 +1,12 @@
 package com.bridzelab.CabInvoiceGenerator;
-
 import org.junit.jupiter.api.Assertions;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CabInvoiceGeneratorTest {
     CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
 
-    /**
-     * test for calculate fare *UC1
-     */
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         double distance = 2.0;
@@ -18,27 +15,22 @@ public class CabInvoiceGeneratorTest {
         Assertions.assertEquals(25, fare, 0.0);
     }
 
-    /**
-     * test for minimum input
-     */
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
         double distance = 0.1;
         int time = 1;
         double fare = cabInvoiceGenerator.calculateFare(distance, time);
-        Assertions.assertEquals(2, fare, 0.0);
+        Assertions.assertEquals(5, fare, 0.0);
     }
 
-    /**
-     * test for multiple rides input *UC2
-     */
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare() {
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
         Ride[] rides = {
                 new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
-        double totalFare = cabInvoiceGenerator.calculateFare(rides);
-        Assertions.assertEquals(27.0, totalFare, 0.0);
+        InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assertions.assertEquals(invoiceSummary, expectedInvoiceSummary);
     }
 }
